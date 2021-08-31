e865085a1757cb1481f3fc5cf64833f1ee188199
Swathi <battulaswathi@gmail.com>
2021-09-01 03:12:47 +0530

    Delete TESAgents directory

--- TESAgents/cleanPyomoTempFiles.bat
+++
@@ -1,7 +0,0 @@
-set TempFiles=C:\Users\swathi\Dropbox\ITDTESPlatform\AMES-V5.0X\DataFiles\PyomoTempFiles
-cd %TempFiles%
-del *.lp
-del *.log
-del *.script
-del *.sol
-del *.dat
--- TESAgents/compileAMES.bat
+++
@@ -1,6 +0,0 @@
-set TSDir=C:\Users\swathi\Dropbox\ITDTESPlatform
-set AMESDir=%TSDir%\AMES-V5.0X\
-
-cd %AMESDir%
-call ant clean
-call ant jar
